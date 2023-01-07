bcd65a18544d7d4a3c3a942fd9b74fb9373d391d
Mo <mohammedzaber@hotmail.com>
2023-01-07 10:24:22 +0000

    did the first part of the challenge which was adding the questions that generate the random password and linking to html to call the function

---
+++ script.js
@@ -0,0 +1,123 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+//First step was to include the prompts for the users password to seee what they want, I asked all the questions for the challenege and simply placed them under the functions getpasswordoptions.
+function getPasswordOptions() {
+passwordLength = prompt("Please choose a password length from atleast 10 characters but no more than 64?")
+lowerCasedCharacters = prompt("Click ok to confirm using lowercase characters")
+upperCasedCharacters = prompt("Click ok to confirm using uppercase characters")
+numericCharacters = prompt("Click ok to confirm using numeric characters")
+specialCharacters = prompt ("Click ok to confirm using speacial characters");
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var lowerCasedCharacters = lowerCasedCharacters[Math.floor(Math.random()*lowerCasedCharacters.length)];
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
